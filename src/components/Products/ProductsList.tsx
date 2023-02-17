6e56d9f958c0d7594167d02790f0b5ceb1c89af4
admin <admin@admins-MacBook-Pro.local>
2023-02-17 16:13:21 +0200

    add destructuring and key prop

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -5,6 +5,7 @@ import productsArray from 'utils/productsArray'
 type Props = {}
 
 type Product = {
+    id: number
     title: string
     description: string
     type: string
@@ -15,21 +16,35 @@ type Product = {
 const ProductsList = (props: Props) => {
     return (
         <>
-            <Typography variant="h3" component="h2" align="center">
+            <Typography
+                variant="h3"
+                component="h2"
+                align="center"
+                sx={{ marginBottom: '30px' }}
+            >
                 List of Products
             </Typography>
             <Grid container spacing={4}>
-                {productsArray.map((product: Product) => (
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ProductsListItem
-                            title={product.title}
-                            description={product.description}
-                            type={product.type}
-                            capacity={product.capacity}
-                            price={product.price}
-                        />
-                    </Grid>
-                ))}
+                {productsArray.map(
+                    ({
+                        id,
+                        title,
+                        description,
+                        capacity,
+                        type,
+                        price,
+                    }: Product) => (
+                        <Grid item xs={12} sm={6} md={4} key={id}>
+                            <ProductsListItem
+                                title={title}
+                                description={description}
+                                type={type}
+                                capacity={capacity}
+                                price={price}
+                            />
+                        </Grid>
+                    )
+                )}
             </Grid>
         </>
     )
