6eae2ab7d930f0dcb28266cb152935e59685ee45
admin <admin@admins-MacBook-Pro.local>
2023-02-17 12:10:04 +0200

    add header,main,footer and app

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import List from './List'
-
-type TitleProps = {
-    name: string
-    text: string
-    year: number
-}
-
-const Title = ({ name, text, year }: TitleProps) => {
-    return (
-        <>
-            <h1>Hello {name}</h1>
-            <p>{text}</p>
-            <div>Year {year}</div>
-        </>
-    )
-}
-
-const App = () => {
-    return (
-        <div className="app">
-            <Title name="App" text="Lorem fdgfd" year={10} />
-            <Title name="React" text="Lorem fdgfd" year={10} />
-            <Title name="TS" text="Lorem fdgfd" year={10} />
-            <List />
-        </div>
-    )
-}
+import App from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
