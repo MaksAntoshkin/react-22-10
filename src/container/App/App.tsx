c4cf80c60cec6a6e79d5d4947f98a0c02345e9e3
admin <admin@admins-MacBook-Pro.local>
2023-02-19 12:22:34 +0200

    add to cart

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -17,10 +17,20 @@ const App = (props: Props) => {
         totalCount: 0,
         totalPrice: 0,
     })
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + count * price,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
+            <button onClick={() => addProductToCart(5, 500)}>
+                Add to Cart
+            </button>
             <Main />
             <Footer />
         </StyledEngineProvider>
