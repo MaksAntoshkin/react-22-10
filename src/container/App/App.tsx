f5c63698779f746a8d5e55224073a2b133b992eb
admin <admin@admins-MacBook-Pro.local>
2023-02-25 11:51:17 +0200

    add ProductsInCart with id

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -7,30 +7,26 @@ import { useState } from 'react'
 
 type Props = {}
 
-type CartDataProps = {
-    totalCount: number
-    totalPrice: number
+type ProductsInCartType = {
+    [id: number]: number
 }
 
 const App = (props: Props) => {
-    const [cartData, setCartData] = useState<CartDataProps>({
-        totalCount: 0,
-        totalPrice: 0,
+    const [productsInCart, setRpoductsInCart] = useState<ProductsInCartType>({
+        1: 5,
+        2: 5,
+        3: 1,
     })
-    const addProductToCart = (count: number, price: number) => {
-        setCartData((prevState) => ({
-            totalCount: prevState.totalCount + count,
-            totalPrice: prevState.totalPrice + count * price,
-        }))
-    }
+
+    const addProductToCart = (count: number, price: number) => {}
 
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header cartData={cartData} />
-            <button onClick={() => addProductToCart(5, 500)}>
+            <Header productsInCart={productsInCart} />
+            {/* <button onClick={() => addProductToCart(5, 500)}>
                 Add to Cart
-            </button>
+            </button> */}
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
