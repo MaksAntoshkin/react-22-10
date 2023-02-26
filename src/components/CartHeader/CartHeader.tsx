b1e8c40423a762b78aedfb0ea58a99860b35bbfd
admin <admin@admins-MacBook-Pro.local>
2023-02-26 10:45:41 +0200

    add title to cart

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,15 +1,17 @@
+import productsArray from 'utils/productsArray'
+
 type Props = {
     productsInCart: {
         [id: number]: number
     }
 }
 const CartHeader = ({ productsInCart }: Props) => {
-    console.log(Object.keys(productsInCart))
     return (
         <div>
             {Object.keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productId}: {productsInCart[parseInt(productId)]}
+                    {productsArray[parseInt(productId) - 1].title}:{' '}
+                    {productsInCart[parseInt(productId)]}
                 </div>
             ))}
         </div>
